d21f889bee4f7c8278645fcd38a7f3ed46423b38
Владислав Ланг <eisec@vk.com>
2025-06-30 18:48:42 +0300

    fix images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'spbremontotdelka.ru',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
